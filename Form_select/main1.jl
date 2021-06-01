d7c1ab602c3612e1c34cfa3d9b49ec1b0297bf62
Giampaolo Liuzzi <giampaolo.liuzzi@iasi.cnr.it>
2021-06-01 11:09:56 +0200

    updated main and main1.jl

--- Form_select/main1.jl
+++ Form_select/main1.jl
@@ -4,10 +4,10 @@ using Distributed
 
 using mod_run
 
-nloc       = 10					# number of local searches to determine first GUB
-MAXNODES   = Inf				# maximum number of nodes to be explored
-TOLGAP     = 1.e-5				# tolerance for node fathoming and stop
-time_limit = 1800				# time limit
+nloc       = 10                 # number of local searches to determine first GUB
+MAXNODES   = Inf                # maximum number of nodes to be explored
+TOLGAP     = 1.e-5              # tolerance for node fathoming and stop
+time_limit = 1800               # time limit
 probfile   = "qp20_10_1_2.mat"  # problem file in folder ../randqp
 
 addprocs(10)
